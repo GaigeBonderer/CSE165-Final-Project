525e49dd981e1fa5a8e89ffebf1901e917765c9e
GaigeBonderer <gbonderer@ucmerced.edu>
2024-04-28 15:03:12 -0700

    Update main.cpp

--- BasicOpenGLProject/BasicOpenGLProject/main.cpp
+++ BasicOpenGLProject/BasicOpenGLProject/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <list>
+#include <memory>
 
 //-----------------------------------------------------------------------------
 // CALLBACK DOCUMENTATION
